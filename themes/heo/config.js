9837db698526b09219285d980641e1494b3e81c7
wanwuzhishi <119481322+wanwuzhishi@users.noreply.github.com>
2024-10-10 13:54:15 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-10-10', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
@@ -18,11 +18,11 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: '万物之时的个人博客',
+  HEO_HERO_TITLE_1: '分享编程学习',
+  HEO_HERO_TITLE_2: '与有趣的各类资源',
+  HEO_HERO_TITLE_3: 'WWZS,US.KG',
+  HEO_HERO_TITLE_4: 'Hexo',
+  HEO_HERO_TITLE_5: '从零开始搭建Hexo博客',
   HEO_HERO_TITLE_LINK: 'https://wwzs.us.kg',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
